d3281a3a592282fe750e3034205f4f7a12c38f24
Anshuman patel <anshuman.patel@koshex.com>
2024-11-11 18:04:10 +0530

    v1

---
+++ Example/Pods/Target Support Files/MyLibraryCP/MyLibraryCP-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_MyLibraryCP : NSObject
+@end
+@implementation PodsDummy_MyLibraryCP
+@end
---
+++ Example/Pods/Target Support Files/Pods-MyLibraryCP_Example/Pods-MyLibraryCP_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MyLibraryCP_Example : NSObject
+@end
+@implementation PodsDummy_Pods_MyLibraryCP_Example
+@end
